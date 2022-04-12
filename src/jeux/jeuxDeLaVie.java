a3ff18d121a1a2d879242eaf1606731446f1f428
Jauzion Rémi <remi.jauzion@iut-rodez.fr>
2022-04-12 15:01:31 +0200

    projet initial

---
+++ src/jeux/jeuxDeLaVie.java
@@ -0,0 +1,13 @@
+/*
+ * jeuxDeLaVie.java                                  12 avr. 2022
+ * IUT de Rodez, Info1 2021-2022 Groupe 3, pas de copyright 
+ */
+package jeux;
+
+/** TODO commenter la responsabilité de cette classe
+ * @author remi.jauzion
+ *
+ */
+public class jeuxDeLaVie {
+
+}
